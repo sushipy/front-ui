185c4a2357ff417184ee3c7d5a2a3372744214e1
KimuraShinya <k.shinya76@gmail.com>
2015-12-13 00:50:10 +0900

    一覧表示、詳細表示、イベント登録、参加登録の動作確認がとれてます

---
+++ EventDAO.py
@@ -0,0 +1,118 @@
+# coding: utf-8
+
+import MySQLdb
+
+class Event:
+    def __init__(self):
+        self._connect()
+        return
+
+    def _connect(self):
+        self.connection = MySQLdb.connect(\
+            host="192.168.56.155", \
+            db="bmi22", \
+            user="root", \
+            passwd="password")
+        return
+
+    def _disconnect(self):
+        self.connection.close()
+        return
+
+    def _get_cursor(self):
+        self.connection.cursorclass = MySQLdb.cursors.DictCursor
+        return self.connection.cursor()
+
+    # 新規イベント作成
+    def create(self, query_array):
+        cursor = self._get_cursor()
+        stmt = ("insert into event "
+#                "(date, ftime, time, title, room, promotor_name, promotor_mail, capacity, descri, agenda, note, participant) "
+#                "values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+                "(date, ftime, time, title, room, promotor_name, promotor_mail, capacity, descri, agenda, note) "
+                "values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+        data = query_array
+        cursor.execute(stmt, data)
+        self.connection.commit()
+        cursor.close()
+        self._disconnect()
+        return
+
+    # 全イベント一覧取得
+    def find_event_all(self):
+        cursor = self._get_cursor()
+#        stmt = "select date, ftime, time, title, room, promotor_name, promotor_mail, capacity, descri, agenda, note, participant from event"
+        stmt = "select id, date, ftime, time, title, room, promotor_name, promotor_mail, capacity, descri, agenda, note from event"
+
+        cursor.execute(stmt)
+        rows = cursor.fetchall()
+        cursor.close()
+        self._disconnect()
+        return rows
+
+    # イベント更新
+    # 排他制御は未実装・・・
+    def update(self, id, query_array):
+        cursor = self._get_cursor()
+        stmt = ("update event "
+                "set date=%s, ftime=%s, time=%s, title=%s, room=%s, promotor_name=%s, promotor_mail=%s, capacity=%s, descri=%s, agenda=%s, note=%s, participant=%s "
+                "where id=%s")
+        query_array.append(id)
+        data = query_array
+        cursor.execute(stmt, data)
+        self.connection.commit()
+        cursor.close()
+        self._disconnect()
+        return
+
+    # イベント参加
+    def participate(self, id, participant_name):
+        cursor = self._get_cursor()
+
+        # 更新前のイベント参加人数取得
+        stmt_select = ("select participant from event "
+                       "where id=%s")
+        cursor.execute(stmt_select, id)
+        row = cursor.fetchall()
+
+        # 参加者名登録
+        stmt_insert = ("insert into participate_history "
+                       "(id, participant_name) "
+                       "values (%s, %s)")
+        data = [id, participant_name]
+        cursor.execute(stmt_insert, data)
+
+        # 参加者人数更新
+        stmt_update = ("update event "
+                       "set participant=%s "
+                       "where id=%s")
+        participant = row[0]["participant"] + 1
+        data = [participant, id]
+        cursor.execute(stmt_update, data)
+
+        self.connection.commit()
+        cursor.close()
+        self._disconnect()
+        return
+
+    # 全参加者リスト取得
+    def find_participants_all(self, id):
+        cursor = self._get_cursor()
+        stmt = "select event.title, participant_name from participate_history inner join event on (participate_history.id = event.id)"
+        cursor.execute(stmt)
+        rows = cursor.fetchall()
+        cursor.close()
+        self._disconnect()
+        return rows
+
+    # イベント検索（イベント名で）
+    def find_by_name(self, name):
+        cursor = self._get_cursor()
+        stmt = ("select date, ftime, time, title, room, promotor_name, promotor_mail, capacity, descri, agenda, note, participant from event "
+                "where title=%s")
+        data = [name]
+        cursor.execute(stmt, data)
+        rows = cursor.fetchall()
+        cursor.close()
+        self._disconnect()
+        return rows
---
+++ ccc.py
@@ -0,0 +1,115 @@
+# coding: UTF-8
+import sys
+import json
+from BaseHTTPServer import BaseHTTPRequestHandler
+from BaseHTTPServer import HTTPServer
+import EventDAO
+import urlparse
+import createHTML
+
+class JsonResponseHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        print "POST"
+
+        parsed_path = urlparse.urlparse(self.path)
+        print parsed_path.path
+
+        if 'create' in parsed_path.path:
+            content_len = int(self.headers.get('content-length'))
+            requestBody = self.rfile.read(content_len).decode('UTF-8')
+            eList = json.loads(requestBody)
+
+            date = eList['date'].encode('utf-8')
+            ftime = eList['ftime'].encode('utf-8')
+            time = eList['time'].encode('utf-8')
+            title = eList['title'].encode('utf-8')
+            room = eList['room'].encode('utf-8')
+            promotor_name = eList['promotor_name'].encode('utf-8')
+            promotor_email = eList['promotor_email'].encode('utf-8')
+            capacity = eList['capacity'].encode('utf-8')
+            descri = eList['descri'].encode('utf-8')
+            agenda = eList['agenda'].encode('utf-8')
+            note = eList['note'].encode('utf-8')
+        
+            event = EventDAO.Event()
+            queryarray = [date, ftime, time, title, room, promotor_name, promotor_email, capacity, descri, agenda, note]
+            
+            event.create(queryarray)
+            
+            self.send_response(200)
+            self.end_headers()
+            print "Create OK"
+        elif 'events' in parsed_path.path:
+            content_len = int(self.headers.get('content-length'))
+            requestBody = self.rfile.read(content_len).decode('UTF-8')
+            eList = json.loads(requestBody)
+
+            event_id = eList['event_id']
+            promotor_email = eList['promotor_email']
+
+            event = EventDAO.Event()
+            event.participate(event_id,promotor_email)
+            self.send_response(200)
+            self.end_headers()
+            print "Sanka OK"
+
+    def do_GET(self):
+        print "GET"
+
+        event = EventDAO.Event()
+        eventlist = event.find_event_all()
+        cnt = len(eventlist)
+
+        parsed_path = urlparse.urlparse(self.path)
+        print parsed_path.path
+
+        if 'showlist' in parsed_path.path:
+            print "showlist"
+
+            makeHTML = createHTML.createHTML()
+            res = makeHTML.showlistHTML(cnt,eventlist)
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html;charset=utf-8')
+            self.end_headers()
+            self.wfile.write(res)
+        elif 'events' in parsed_path.path:
+           print "detail"
+
+           eventID = parsed_path.path[8:]
+           makeHTML = createHTML.createHTML()
+           res = makeHTML.detailHTML(eventID,eventlist)
+
+           self.send_response(200)
+           self.send_header('Content-type', 'text/html;charset=utf-8')
+           self.end_headers()
+           self.wfile.write(res)
+
+    def do_PUT(self):
+        print "PUT"
+
+        parsed_path = urlparse.urlparse(self.path)
+        print parsed_path.path
+
+        if parsed_path.path.find(''):
+            print "madamada"
+
+        elif parsed_path.path.find(''):
+            print "madamada"
+
+    def do_DELETE(self):
+        print "DELETE"
+
+        parsed_path = urlparse.urlparse(self.path)
+        print parsed_path.path
+
+        if parsed_path.path.find(''):
+            print "madamada"
+        elif parsed_path.path.find(''):
+            print "madamada"
+
+if __name__ == '__main__':
+    server = HTTPServer(('', 8001), JsonResponseHandler)
+    print 'Starting server, use <Ctrl-C> to stop'
+    server.serve_forever()
